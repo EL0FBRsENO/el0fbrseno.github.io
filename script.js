bedba181a4101b05e239ed083e60c8477ee8787a
brenio <72355208+EL0FBRsENO@users.noreply.github.com>
2022-12-31 20:02:07 -0300

    among us style and working imposter randomizer
    working on the sus leaderboard

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function pickRandomArray(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
@@ -11,7 +10,16 @@ function pickRandomInt(int) {
     return randomIndex;
 }
 
-let impostors = [];
+function hover() {
+    let audio = document.getElementById("hoverSound");
+    audio.play();
+}
+function active() {
+    var audio = document.getElementById("clickSound");
+    audio.play();
+}
+
+var impostors = [];
 
 impostors.push("/images/impostors/red.png");
 impostors.push("/images/impostors/blue.png");
@@ -23,17 +31,42 @@ impostors.push("/images/impostors/yellow.png");
 impostors.push("/images/impostors/pink.png");
 impostors.push("/images/impostors/white.png");
 impostors.push("/images/impostors/black.png");
+impostors.push("/images/impostors/amogus.png");
+
+var sus_percent = [];
+for (var i = 0; i < impostors.length; ++i) {
+    sus_percent += impostors[i].substring(18, impostors[i].length).replace(".png", "");
+}
+console.log(sus_percent);
+
+var div = document.getElementById("spanLabel");
 
 function clicar() {
+    ejectButton.disabled = false;
     var container = document.getElementById("photos");
+    var percentage = document.getElementById("blue");
+    var audio = document.getElementById("clickSound");
+    audio.play();
+
+    percentage.innerHTML = sus_percent.includes("")
     
     var img = new Image();
     img.src = pickRandomArray(impostors);
     if (container.hasChildNodes()) {
         container.removeChild(container.firstElementChild);
         container.appendChild(img);
+        var split = img.src.substring(39, img.src.length).replace(".png", "");
+        div.innerHTML = `${split} is sus`;
     }else {
         container.appendChild(img);
+        var split = img.src.substring(39, img.src.length).replace(".png", "");
+        div.innerHTML = `${split} is sus`;
     }
 
+
 }
+
+
+var ejectButton = document.getElementById("ejectButton");
+
+ejectButton.disabled = true;
